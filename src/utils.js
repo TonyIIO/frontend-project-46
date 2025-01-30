e6e394cb710b7ec5429391731447e80647b87706
Anton Polyakovskiy <tony@HOME-PC.>
2025-01-30 12:00:39 +0300

    3

--- src/utils.js
+++ src/utils.js
@@ -14,6 +14,6 @@ const readFile = (filename) => {
   }
 };
 
-const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', filename);
 
 export { readFile, getFixturePath };
