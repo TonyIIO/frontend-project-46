e01ed661e99b478ee6d221f04b48c2824d6c38fd
Anton Polyakovskiy <tony@HOME-PC.>
2025-01-31 23:56:02 +0300

    fixPatch

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,3 +1,28 @@
+// исходдный проход (не прошло тест)
+// import _ from 'lodash';
+
+// eslint-disable-next-line max-len
+// const getIndent = (depth, indentSize = 4, leftOffset = 2) => ' '.repeat(depth * indentSize - leftOffset);
+
+// const stylish = (object, depth = 1) => _.keys(object).reduce((acc, key) => {
+//   const value = object[key];
+//   const indent = getIndent(depth);
+
+//   if (_.isPlainObject(value)) {
+//     const nested = stylish(value, depth + 1).join('\n');
+//     acc.push(`${indent}${key}: {\n${nested}\n${getIndent(depth, 4, 0)}}`);
+//   } else {
+//     acc.push(`${indent}${key}: ${value}`);
+//   }
+
+//   return acc;
+// }, []);
+
+// const formatStylish = (object) => `{\n${stylish(object).join('\n')}\n}`;
+
+// export default formatStylish;
+
+// добавление двух пробелов(не прошло тест)
 import _ from 'lodash';
 
 const replacer = ' ';
@@ -26,23 +51,52 @@ export default formatStylish;
 
 // import _ from 'lodash';
 
-// eslint-disable-next-line max-len
-// const getIndent = (depth, indentSize = 4, leftOffset = 2) => ' '.repeat(depth * indentSize - leftOffset);
+// const replacer = ' ';
+// const spacesCount = 4;
 
-// const stylish = (object, depth = 1) => _.keys(object).reduce((acc, key) => {
-//   const value = object[key];
-//   const indent = getIndent(depth);
+// const indent = (depth) => replacer.repeat(depth * spacesCount - 2);
+// const bracketIndent = (depth) => replacer.repeat((depth - 1) * spacesCount);
 
-//   if (_.isPlainObject(value)) {
-//     const nested = stylish(value, depth + 1).join('\n');
-//     acc.push(`${indent}${key}: {\n${nested}\n${getIndent(depth, 4, 0)}}`);
-//   } else {
-//     acc.push(`${indent}${key}: ${value}`);
+// const stringify = (value, depth) => {
+//   if (!_.isObject(value)) {
+//     return `${value}`;
 //   }
 
-//   return acc;
-// }, []);
+//   const lines = Object.entries(value).map(
+//     ([key, val]) => `${indent(depth + 1)}  ${key}: ${stringify(val, depth + 1)}`,
+//   );
 
-// const formatStylish = (object) => `{\n${stylish(object).join('\n')}\n}`;
+//   return `{\n${lines.join('\n')}\n${bracketIndent(depth + 1)}}`;
+// };
 
-// export default formatStylish;
+// const stylish = (diffTree) => {
+//   const iter = (node, depth) => {
+// eslint-disable-next-line max-len
+//     const makeLine = (sign, key, value) => `${indent(depth)}${sign} ${key}: ${stringify(value, depth)}`;
+
+//     return node.flatMap((item) => {
+//       switch (item.type) {
+//         case 'added':
+//           return makeLine('+', item.key, item.value);
+//         case 'removed':
+//           return makeLine('-', item.key, item.value);
+//         case 'unchanged':
+//           return makeLine(' ', item.key, item.value);
+//         case 'changed':
+//           return [
+//             makeLine('-', item.key, item.oldValue),
+//             makeLine('+', item.key, item.newValue),
+//           ];
+//         case 'nested':
+// eslint-disable-next-line max-len
+//           return `${indent(depth)}  ${item.key}: {\n${iter(item.children, depth + 1).join('\n')}\n${bracketIndent(depth)}  }`;
+//         default:
+//           throw new Error(`Unknown type: ${item.type}`);
+//       }
+//     });
+//   };
+
+//   return `{\n${iter(diffTree, 1).join('\n')}\n}`;
+// };
+
+// export default stylish;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
-import { fileURLToPath } from 'url';
 import fs from 'fs';
 import path from 'path';
 import parser from './parsers.js';
 import differences from './differences.js';
 import formater from './formatters/index.js';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-// const getFullPath = (filepath) => path.resolve(__dirname, '..', '__fixtures__', filepath);
-const getFullPath = (filepath) => (path.isAbsolute(filepath)
-  ? filepath : path.resolve(__dirname, '..', '__fixtures__', filepath));
+const getFullPath = (filepath) => (
+  path.isAbsolute(filepath)
+    ? filepath
+    : path.resolve(process.cwd(), filepath)
+);
+//
+const getData = (filepatch) => fs.readFileSync(filepatch, 'utf-8');
 
+const gendiff = (filepath1, filepath2, fileType = 'stylish') => {
+  const filePatch1 = getFullPath(filepath1);
+  const filePatch2 = getFullPath(filepath2);
+  const obj1 = parser(getData(filePatch1), filePatch1);
+  const obj2 = parser(getData(filePatch2), filePatch2);
+  const diff = differences(obj1, obj2);
+  return formater(diff, fileType);
+};
+
+export default gendiff;
+
+// import { fileURLToPath } from 'url';
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+// const getFullPath = (filepath) => path.resolve(__dirname, '..', '__fixtures__', filepath);
 // const readFile = (filepath) => {
 //   const fullPath = getFixturePath(filepath);
 //   if (!fs.existsSync(fullPath)) {
@@ -27,15 +43,3 @@ const getFullPath = (filepath) => (path.isAbsolute(filepath)
 //   return parser(getData, filepath);
 
 // const getFullPath = (filepatch) => path.resolve(process.cwd(), filepatch);
-const getData = (filepatch) => fs.readFileSync(filepatch, 'utf-8');
-
-const gendiff = (filepath1, filepath2, fileType = 'stylish') => {
-  const filePatch1 = getFullPath(filepath1);
-  const filePatch2 = getFullPath(filepath2);
-  const obj1 = parser(getData(filePatch1), filePatch1);
-  const obj2 = parser(getData(filePatch2), filePatch2);
-  const diff = differences(obj1, obj2);
-  return formater(diff, fileType);
-};
-
-export default gendiff;
