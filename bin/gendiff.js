b54fc3213e38cfcc1f1753e93532cfc3275a137e
Anton Polyakovskiy <tony@HOME-PC.>
2025-02-02 00:04:34 +0300

    18

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,6 +5,8 @@ import { Command } from 'commander';
 import gendiff from '../src/index.js';
 
 const program = new Command();
+const defaultFile1 = '__fixtures__/file1.json';
+const defaultFile2 = '__fixtures__/file2.json';
 
 program
   .name('gendiff')
@@ -13,7 +15,7 @@ program
   .version('1.0.0')
   .arguments('<filepath1> <filepath2>')
   .option('-f, --format [type]', 'output format', 'stylish', 'plain', 'json')
-  .action((filepath1, filepath2) => {
+  .action((filepath1 = defaultFile1, filepath2 = defaultFile2) => {
     const options = program.opts();
     console.log(gendiff(filepath1, filepath2, options.format));
   });
--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ import formater from './formatters/index.js';
 
 const getFullPath = (filepath) => path.resolve(process.cwd(), filepath);
 
-const getData = (filepatch) => fs.readFileSync(filepatch, 'utf-8');
+const getData = (filepath) => fs.readFileSync(filepath, 'utf-8');
 
 const gendiff = (filepath1, filepath2, fileType = 'stylish') => {
-  const filePatch1 = getFullPath(filepath1);
-  const filePatch2 = getFullPath(filepath2);
-  const obj1 = parser(getData(filePatch1), filePatch1);
-  const obj2 = parser(getData(filePatch2), filePatch2);
+  const filePath1 = getFullPath(filepath1);
+  const filePath2 = getFullPath(filepath2);
+  const obj1 = parser(getData(filePath1), filePath1);
+  const obj2 = parser(getData(filePath2), filePath2);
   const diff = differences(obj1, obj2);
   return formater(diff, fileType);
 };
